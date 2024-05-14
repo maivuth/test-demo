7ac566e18f125f8be6b54c4fec4722812466e7ff
maivuth <maivu@kms-technology.com>
2024-05-14 16:25:29 +0700

    1st commit

---
+++ Test Suites/ts1 - demo.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ts1 - demo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7dc7f3ba-afe9-4269-b375-491db784ce1c</testSuiteGuid>
+   <testCaseLink>
+      <guid>4f82f6c2-4b25-46ba-98c6-cf84d0a7c977</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/fd 1/New Test Case - test demo 1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
